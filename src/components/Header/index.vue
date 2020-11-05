1c6633ea6bce9d0f165de49aee58fa5b8828de34
Zhitong Ren <zhitongren@Zhitongs-MacBook-Pro.local>
2020-11-05 23:44:15 +0800

    code init

---
+++ src/components/Header/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <header>
+      <span>
+        Basic Communication
+      </span>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="less" scoped>
+.header {
+  background-color: @default-blue;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// init element-ui components
+import "@/common/initElement";
 
 Vue.config.productionTip = false;
 
